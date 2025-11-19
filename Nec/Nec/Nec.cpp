1c8e4386a7616f5e58dac7621cec1b05a68563a8
Berkay Uzun <berkyuzun@gmail.com>
2025-11-19 21:06:21 +0300

    first commit

---
+++ Nec/Nec/Nec.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+
+
+// implicitly declared
+
+/*
+* not declared 
+* tanımlamamak
+* 
+* user declared
+* x();
+* y()=Default;
+* z()=delete;
+* 
+* implicitly declared
+* defaulted
+* deleted
+* 
+*/
+
+
+int main()
+{
+	
+}
+ 
